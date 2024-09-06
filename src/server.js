c3df2e29bcf51a901b2cae5d0503bb9b70106e16
Uday Rana <uday7453@gmail.com>
2024-09-06 19:02:50 -0400

    Add debug log for env vars

--- src/server.js
+++ src/server.js
@@ -13,6 +13,9 @@ const port = parseInt(process.env.PORT || '8080', 10);
 // Start a server listening on this port
 const server = stoppable(
   app.listen(port, () => {
+    // Log environment variables for debugging
+    logger.debug(process.env);
+
     // Log a message that the server has started, and which port it's using.
     logger.info(`Server started on port ${port}`);
   })
