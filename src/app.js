9085e7c837db947b06b524fc6791ed583f1bd158
Uday R <82779467+uday-rana@users.noreply.github.com>
2024-09-11 01:41:14 -0400

    Secure routes with Passport.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
+const passport = require('passport');
+const authenticate = require('./auth');
 
 const logger = require('./logger');
 const pino = require('pino-http')({
@@ -23,6 +25,10 @@ app.use(cors());
 
 // Use gzip/deflate compression middleware
 app.use(compression());
+
+// Set up our passport authentication middleware
+passport.use(authenticate.strategy());
+app.use(passport.initialize());
 
 // Define our routes
 app.use('/', require('./routes'));
