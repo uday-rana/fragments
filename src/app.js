bf1482aad25a00839413cf043cbd5cbfeb8bb7fb
Uday R <uday7453@gmail.com>
2024-09-19 04:55:06 -0400

    Refactor to use response.js functions

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const compression = require('compression');
 const passport = require('passport');
 const authenticate = require('./auth');
+const { createErrorResponse } = require('./response');
 
 const logger = require('./logger');
 const pino = require('pino-http')({
@@ -35,13 +36,7 @@ app.use('/', require('./routes'));
 
 // Add 404 middleware to handle any requests for resources that can't be found
 app.use((req, res) => {
-  res.status(404).json({
-    status: 'error',
-    error: {
-      message: 'not found',
-      code: 404,
-    },
-  });
+  res.status(404).json(createErrorResponse(404, 'not found'));
 });
 
 // Add error-handling middleware to deal with anything else
@@ -57,13 +52,7 @@ app.use((err, req, res, next) => {
     logger.error({ err }, `Error processing request`);
   }
 
-  res.status(status).json({
-    status: 'error',
-    error: {
-      message,
-      code: status,
-    },
-  });
+  res.status(status).json(createErrorResponse(status, message));
 });
 
 // Export our `app` so we can access it in server.js
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,9 @@ const router = express.Router();
 // Our authentication middleware
 const { authenticate } = require('../auth');
 
+// Success response generator
+const { createSuccessResponse } = require('../response');
+
 /**
  * Expose all of our API routes on /v1/* to include an API version.
  */
@@ -22,13 +25,14 @@ router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
   // Send a 200 'OK' response
-  res.status(200).json({
-    status: 'ok',
-    author,
-    // Use your own GitHub URL for this!
-    githubUrl: 'https://github.com/uday-rana/fragments',
-    version,
-  });
+  res.status(200).json(
+    createSuccessResponse({
+      author,
+      // Use your own GitHub URL for this!
+      githubUrl: 'https://github.com/uday-rana/fragments',
+      version,
+    })
+  );
 });
 
 module.exports = router;
