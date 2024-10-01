02ed215cd5295660d665544c40879ffe933491e5
Uday R <82779467+uday-rana@users.noreply.github.com>
2024-10-01 12:14:35 -0400

    feat: add tests for POST /v1/fragments

---
+++ tests/unit/post.test.js
@@ -0,0 +1,92 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+const Fragment = require('../../src/model/fragment');
+
+// Wait for a certain number of ms. Feel free to change this value
+// if it isn't long enough for your test runs. Returns a Promise.
+const wait = async (ms = 10) => new Promise((resolve) => setTimeout(resolve, ms));
+
+describe('POST /v1/fragments', () => {
+  // test conditions:
+  // - auth: authenticated/unauthenticated/no user
+  // - file: raw binary data (buffer)/non raw binary data (not buffer)/no file
+  // - content-type header: supported/not supported/not set
+  describe('happy path', () => {
+    test('authenticated users posting raw binary data of supported content-type get successful response', async () => {
+      const rawData = Buffer.from('hello');
+      const dateBeforeReq = new Date();
+      await wait();
+      const res = await request(app)
+        .post('/v1/fragments')
+        .auth('user1@email.com', 'password1')
+        .type('plain/text')
+        .send(rawData);
+      expect(res.statusCode).toBe(201);
+      expect(res.body.status).toBe('ok');
+      expect(res.body.fragment).toBeInstanceOf(Fragment);
+      // TODO: complete this test using src/hash.js once implemented
+      // expect(res.body.fragment.ownerId).toEqual()
+      expect(res.body.fragment.size).toStrictEqual(Buffer.byteLength(rawData));
+      expect(Date.parse(res.body.fragment.created)).toBeGreaterThan(dateBeforeReq);
+      expect(Date.parse(res.body.fragment.updated)).toBeGreaterThan(dateBeforeReq);
+      // TODO: update to check for full url with API_KEY env once implemented
+      expect(res.headers.location.endsWith(res.body.fragment.id)).toBeTruthy();
+    });
+  });
+
+  describe('erronous data in request body', () => {
+    // Could maybe combine these tests, depends on how the request looks when no data is sent
+    test('non-buffer data sent in request body', async () => {
+      const rawData = 'hello';
+      const res = await request(app)
+        .post('/v1/fragments')
+        .auth('user1@email.com', 'password1')
+        .type('plain/text')
+        .send(rawData);
+      expect(res.statusCode).toBe(400);
+    });
+
+    test('no data sent in request body', async () => {
+      const res = await request(app)
+        .post('/v1/fragments')
+        .auth('user1@email.com', 'password1')
+        .type('plain/text');
+      expect(res.statusCode).toBe(400);
+    });
+  });
+
+  describe('erronous content-type header in request', () => {
+    test('unsupported content-type set in request header', async () => {
+      const rawData = Buffer.from('hello');
+      const res = await request(app)
+        .post('/v1/fragments')
+        .auth('user1@email.com', 'password1')
+        .type('hmmm')
+        .send(rawData);
+      expect(res.statusCode).toBe(415);
+    });
+
+    test('content-type not set in request header', async () => {
+      const rawData = Buffer.from('hello');
+      const res = await request(app)
+        .post('/v1/fragments')
+        .auth('user1@email.com', 'password1')
+        .send(rawData);
+      expect(res.statusCode).toBe(415);
+    });
+  });
+
+  describe('unauthorized requests', () => {
+    // If the request is missing the Authorization header, it should be forbidden
+    test('unauthenticated requests are denied', () =>
+      request(app).post('/v1/fragments').expect(401));
+
+    // If the wrong username/password pair are used (no such user), it should be forbidden
+    test('incorrect credentials are denied', () =>
+      request(app)
+        .post('/v1/fragments')
+        .auth('invalid@email.com', 'incorrect_password')
+        .expect(401));
+  });
+});
