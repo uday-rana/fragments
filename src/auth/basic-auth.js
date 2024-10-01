3c32dcd936df0d6c0c139cc9cd09f8034b86b00d
Uday R <82779467+uday-rana@users.noreply.github.com>
2024-10-01 17:14:46 -0400

    feat: implement custom auth middleware

--- src/auth/basic-auth.js
+++ src/auth/basic-auth.js
@@ -2,8 +2,8 @@
 // https://github.com/http-auth/http-auth-passport
 
 const auth = require('http-auth');
-const passport = require('passport');
 const authPassport = require('http-auth-passport');
+const authorize = require('./auth-middleware');
 
 // We expect HTPASSWD_FILE to be defined.
 if (!process.env.HTPASSWD_FILE) {
@@ -19,4 +19,4 @@ module.exports.strategy = () =>
     })
   );
 
-module.exports.authenticate = () => passport.authenticate('http', { session: false });
+module.exports.authenticate = () => authorize('http');
--- src/auth/cognito.js
+++ src/auth/cognito.js
@@ -2,10 +2,9 @@
 // Identity Token provided by Cognito. The token will be
 // parsed from the Authorization header (i.e., Bearer Token).
 
-const passport = require('passport');
 const BearerStrategy = require('passport-http-bearer').Strategy;
 const { CognitoJwtVerifier } = require('aws-jwt-verify');
-
+const authorize = require('./auth-middleware');
 const logger = require('../logger');
 
 // We expect AWS_COGNITO_POOL_ID and AWS_COGNITO_CLIENT_ID to be defined.
@@ -58,4 +57,4 @@ module.exports.strategy = () =>
     }
   });
 
-module.exports.authenticate = () => passport.authenticate('bearer', { session: false });
+module.exports.authenticate = () => authorize('bearer');
