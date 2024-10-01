829027005d94e90f854ee5bd604d4d6ae1fd1cda
Uday R <82779467+uday-rana@users.noreply.github.com>
2024-10-01 19:21:46 -0400

    fix: await and try/catch db calls, return on res.json(), format logs

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -5,36 +5,43 @@ const { createSuccessResponse, createErrorResponse } = require('../../response')
 /**
  * Creates a new fragment for the current user (i.e., authenticated user)
  */
-module.exports = (req, res) => {
-  logger.debug({ req }, `Incoming request: POST /v1/fragments`);
-  try {
-    // handle incorrect content-type or malformed request body
-    if (!Buffer.isBuffer(req.body)) {
-      res.status(415).json(createErrorResponse(415, 'unsupported media type'));
-    }
-    const newFragmentOwnerId = req.user;
-    const newFragmentType = req.headers['content-type'];
-    const newFragmentSize = Buffer.byteLength(req.body);
-    const newFragment = new Fragment({
-      ownerId: newFragmentOwnerId,
-      type: newFragmentType,
-      size: newFragmentSize,
-    });
-    newFragment.save();
-    newFragment.setData(req.body);
-    logger.debug(
-      `constructing URL: ${process.env?.API_URL || req.headers?.host}/v1/fragments/${newFragment.id}`
-    );
-    const locationURL = new URL(
-      `/v1/fragments/${newFragment.id}`,
-      `https://${process.env?.API_URL || req.headers?.host}`
-    );
-    res
-      .status(201)
-      .set('location', locationURL)
-      .json(createSuccessResponse({ fragment: newFragment }));
-  } catch (error) {
-    logger.error(`error on POST /v1/fragments`, { error });
-    res.status(500).json(createErrorResponse(500, 'server error'));
+module.exports = async (req, res) => {
+  logger.debug({ req, reqBody: req.body }, `Incoming request: POST /v1/fragments`);
+  // handle incorrect content-type or malformed request body
+  if (!Buffer.isBuffer(req.body)) {
+    return res.status(415).json(createErrorResponse(415, 'Unsupported media type'));
   }
+  const newFragmentOwnerId = req.user;
+  const newFragmentType = req.headers['content-type'];
+  const newFragmentSize = Buffer.byteLength(req.body);
+  const newFragment = new Fragment({
+    ownerId: newFragmentOwnerId,
+    type: newFragmentType,
+    size: newFragmentSize,
+  });
+  try {
+    await newFragment.save();
+  } catch (error) {
+    logger.error({ error }, `Error saving fragment`);
+    // we verify input so it can only be a server error
+    return res.status(500).json(createErrorResponse(500, `Error saving fragment`));
+  }
+  try {
+    await newFragment.setData(req.body);
+  } catch (error) {
+    logger.error({ error }), `Error saving fragment data`;
+    // we verify input so it can only be a server error
+    return res.status(500).json(createErrorResponse(500, `Error saving fragment data`));
+  }
+  logger.debug(
+    `Constructing URL: ${process.env?.API_URL || req.headers?.host}/v1/fragments/${newFragment.id}`
+  );
+  const locationURL = new URL(
+    `/v1/fragments/${newFragment.id}`,
+    `https://${process.env?.API_URL || req.headers?.host}`
+  );
+  return res
+    .status(201)
+    .set('location', locationURL)
+    .json(createSuccessResponse({ fragment: newFragment }));
 };
