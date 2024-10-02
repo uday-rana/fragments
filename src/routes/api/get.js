4aab5fec4415af9c552ebadd241f99cebe79da97
Uday R <82779467+uday-rana@users.noreply.github.com>
2024-10-01 21:48:17 -0400

    feat: implement GET /v1/fragments

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,14 +1,19 @@
-const { createSuccessResponse } = require('../../response');
+const logger = require('../../logger');
+const { Fragment } = require('../../model/fragment');
+const { createSuccessResponse, createErrorResponse } = require('../../response');
 
 /**
  * Get a list of fragments for the current user
  */
-module.exports = (req, res) => {
-  // TODO: this is just a placeholder. To get something working, return an empty array...
-  res.status(200).json(
-    createSuccessResponse({
-      // TODO: change me
-      fragments: [],
-    })
-  );
+module.exports = async (req, res) => {
+  logger.debug({ req, reqBody: req.body }, `Incoming request: GET /v1/fragments`);
+  try {
+    const result = await Fragment.byUser(req.user);
+    logger.debug({ result }, `Retreived fragments for user`);
+    return res.status(200).json(createSuccessResponse({ fragments: result }));
+  } catch (error) {
+    logger.error({ error }, `Error getting fragments for user`);
+    // We verify input so any error can only be a server error
+    return res.status(500).json(createErrorResponse(500, 'Error getting fragments for user'));
+  }
 };
