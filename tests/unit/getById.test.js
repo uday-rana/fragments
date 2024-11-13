bb87530053ace391ddf7147068beadc589e70abb
Uday R <82779467+uday-rana@users.noreply.github.com>
2024-11-12 19:00:49 -0500

    test: fix type conversion test not expecting anything

--- tests/unit/getById.test.js
+++ tests/unit/getById.test.js
@@ -37,16 +37,20 @@ describe('GET /v1/fragments/:id', () => {
   describe('type conversion', () => {
     test('markdown is successfully converted to html', async () => {
       const rawData = Buffer.from('# hello');
+
       const responseFromPOST = await request(app)
         .post('/v1/fragments')
         .auth('user1@email.com', 'password1')
         .type('text/markdown')
         .send(rawData);
+
       const responseFromGET = await request(app)
         .get(`/v1/fragments/${responseFromPOST.body.fragment.id}.html`)
         .auth('user1@email.com', 'password1');
 
-      console.log(responseFromGET.statusCode, responseFromGET.statusMessage);
+      expect(responseFromGET.headers['content-type']).toMatch(/^text\/html(?:;.*)?$/);
+      expect(responseFromGET.headers['content-length']).toMatch('15');
+      expect(responseFromGET.text).toEqual('<h1>hello</h1>\n');
     });
   });
 });
